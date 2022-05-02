3e1f191f88353727e8791793d98b3f264448b433
Thomas Moraes <tmoraes2020@fau.edu>
2022-05-02 01:08:18 -0300

    Second commit: Laying the groundwork for the Watchlist, to be updated throughout the project.

--- backend/movieserver.js
+++ backend/movieserver.js
@@ -124,6 +124,8 @@ app.get('/rating', function(req, res) {
 })
 
 //Top250Movies IMDB API
+//needs editing so watchlists are unique for each login
+//each login will have their own collection
 app.get('/top250', function(req, res) {
 
     //for debugging (inside movie search route)
@@ -151,15 +153,206 @@ app.get('/top250', function(req, res) {
 //Watchlist (MongoDB): ----------------------------------------------------
 
 //Add route method
-//adds a movie to the watchlist collection in MongoDB given the movie ID
+//adds a movie to the watchlist collection in MongoDB given the movie ID and information
 //a movie should be added when clicked on a button for the frontend, sending the movie's ID
+//the button will be on the movies' description page
 app.post('/addToWatchlist', function(req, res) {
+    //using a MongoDB collection for Watchlist:
+    var uri = "mongodb://127.0.0.1:27017/";
+    //connecting to database
+    MongoClient.connect(uri, {useUnifiedTopology: true}, function(err, client) {
+    if (err) {
+      console.log("database connection POST error");
+      return res.status(500).send({"message": "Error: Could not connect to database!"});
+    }
 
+    //connecting to Movie Theather database
+    //contains a watchlist collection for each user
+    var dbo = client.db("movieTheaterDB");
+    
+    //preventing duplicates:
+    //query parameters for duplicate search (already in the watchlist)
+    var query = {"_id": parseInt(req.body.movie_id)};
+ 
+    //searching the database for matches using first_name and last_name
+    dbo.collection("watchlist").find(query).toArray(function(err2, queryRes) {
+      if (err2) {
+        //something went wrong
+        console.log("POST query error!");
+        return res.status(500).send({"message": "POST Error: query error!"});
+      }
+      else if (queryRes.length > 0) {
+        //matches were found
+        console.log("This movie is already in the watchlist:", queryRes.length);
+        client.close();
+        return res.status(400).send({"message": "Error: Found " + queryRes.length + " matches!"});
+      }
+      else {
+        //no matches were found, continues to create the document
+        console.log("No duplicates found!");
+        
+        //POST given important information from the movie's description page
+        var studentObj = {
+          "_id": req.body.movie_id,
+          "title": req.body.title,
+          "image": req.body.image,
+          "description": req.body.description,
+          "IMDB_rating": req.body.rating
+        }
+
+        //inserting student record into the students collection
+        dbo.collection("watchlist").insertOne(studentObj, function(err3) {
+          if (err3) {
+            //error message to the console and user client
+            console.log("POST DB Add movie error");
+            return res.status(400).send({"message": "POST Error: Could not Add movie to the watchlist!"});
+          }
+          //no errors, file is created
+          console.log("POST: Movie added to the watchlist!");
+          client.close();
+          return res.status(200).send({"message": "Movie added to the watchlist!"});
+        });
+      }
+    })
+  });
 })
 
+//delete route method
+//deletes a movie from the Watchlist collection in MongoDB given the movie ID
+app.delete('/deleteFromWatchlist', function(req, res) {
 
+    //local database uri
+    var uri = "mongodb://127.0.0.1:27017/";
+  
+    //connecting to MongoDB
+    MongoClient.connect(uri, { useUnifiedTopology: true }, function(err, client) {
+      if (err) {
+        console.log("database connection DELETE error!");
+        return res.status(500).send({"message": "Error: Could not connect to the database!"});
+      }
+      else {
+        //connecting to the Movie Theather database
+        //contains a watchlist collection for each account
+        var dbo = client.db("movieTheaterDB");
+  
+        //query parameters for matching IDs
+        //the movie_id needs to be type casted as a number to find matches in the database
+        var query = {"_id": parseInt(req.params.movie_id)};
+  
+        //deletes the first match with the given query parameters
+        dbo.collection("watchlist").deleteOne(query,function(err2, delRes) {
+          if (err2) {
+            //error message to the console and client
+            console.log("DELETE: deletion error!");
+            return res.status(500).send({"message": "Error: Could not delete movie!"});
+          }
+          //returns an error if no corresponding movie is found
+          else if (delRes.deletedCount === 0) {
+            //movie not found
+            console.log("Movie not found.");
+            client.close();
+            return res.status(400).send({"message": "Error: Movie not found!"});
+          }
+          else {
+            //successfully deleted the movie from watchlist
+            console.log("DELETE: Movie deleted from Watchlist!");
+            client.close();
+            return res.status(200).send({"message": "Movie deleted from Watchlist!"});
+          }
+        });
+      }
+    });
+  }); //end of delete method
 
+//Get Single Movie route method
+//shows a single movie from the Watchlist collection in MongoDB given the movie ID
+//This should re-render the page in react with the movie's description page
+  app.get('/ShowMovieInfo', function(req, res) {
+    //local database uri
+    var uri = "mongodb://127.0.0.1:27017/";
 
+    //connecting to MongoDB
+    MongoClient.connect(uri, { useUnifiedTopology: true }, function(err, client) {
+      if (err) {
+        console.log("database connection ShowMovieInfo error!");
+        return res.status(500).send({"message": "Error: Could not connect to the database!"});
+      }
+      else {
+        //connecting to the database
+        var dbo = client.db("movieTheaterDB");
+  
+        //query parameters for matching IDs
+        //the movie_id needs to be type casted as a number to find matches in the database
+        var query = {"_id": parseInt(req.params.movie_id)};
+  
+        //finds the first match with the given query parameters
+        dbo.collection("watchlist").findOne(query, function(err2, findRes) {
+          if (err2) {
+            //error message to the console and client
+            console.log("ShowMovieInfo: search error!");
+            return res.status(500).send({"message": "Error: Database operation error!"});
+          }
+          if (findRes === null) {
+            //movie not found
+            console.log("Movie not found.");
+            client.close();
+            return res.status(400).send({"message": "Error: Movie not found!"});
+          }
+          else {
+            //turns the found JSON document into a string to return
+            var movieFound = JSON.stringify(findRes, null, 4);
+            //successfully found the movie document, and returns it to the console and client
+            console.log("ShowMovieInfo: Movie info:\n" + movieFound);
+            client.close();
+            return res.status(200).send(movieFound);
+          }
+        });
+      }
+    });
+}); //end of get by id method
+
+//List watchlist route method
+//Returns all movies in the watchlist
+app.get('/showWatchlist', function(req, res) {
+    //local database uri
+    var uri = "mongodb://127.0.0.1:27017/";
+
+    //connecting to MongoDB
+    MongoClient.connect(uri, { useUnifiedTopology: true }, function(err, client) {
+      if (err) {
+        console.log("database connection Watchlist (LIST) error!");
+        return res.status(500).send({"message": "Error: Could not connect to the database!"});
+      }
+      else {
+        //connecting to the database
+        var dbo = client.db("movieTheaterDB");
+
+        //Returns all movies in the watchlist (no query parameter)
+        dbo.collection("students").find().toArray(function(err2, findRes) {
+          if (err2) {
+            //error message to the console and client
+            console.log("Watchlist (LIST): search error!");
+            return res.status(500).send({"message": "Error: Could not find movies!"});
+          }
+          else {
+            //turns the found JSON document into a string to return
+            if (findRes.length > 0) {
+              var moviesFound = JSON.stringify(findRes, null, 4);
+              //successfully found the student record, and returns it to the console and client
+              console.log("Watchlist (LIST): Movies found:\n" + moviesFound);
+              client.close();
+              return res.status(200).send(moviesFound);
+            }
+            else {
+              console.log("Watchlist (LIST): No movies in the database!");
+              client.close();
+              return res.status(400).send({"message": "No movies in the Watchlist!"});
+            }
+          }
+        });
+      }
+    });
+}); //end of GET (matches) route
 
 
 
